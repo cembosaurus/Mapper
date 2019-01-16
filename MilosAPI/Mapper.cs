6caa5f29f7d82df7187facb4abf26d49781ab3d5
cembosaurus <milos.bencek@gmail.com>
2019-01-16 12:10:54 +1100

    Add project files.

---
+++ MilosAPI/Mapper.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+
+namespace MilosAPI
+{
+    public class Mapper
+    {
+
+        /// <summary>
+        /// Mapping properties with identical name and type.
+        /// </summary>
+        /// <param name="_a">Source</param>
+        /// <param name="_b">Destination</param>
+        public Mapper(object _a, object _b)
+        {
+            Mapping(ref _a, ref _b);
+        }
+
+
+        private void Mapping<A, B>(ref A _a, ref B _b)
+        {
+            PropertyInfo[] _aPI = _a.GetType().GetProperties();
+            PropertyInfo[] _bPI = _b.GetType().GetProperties();
+
+            _recursiveB(ref _a, ref _b, ref _aPI, ref _bPI);
+        }
+
+
+        private void _recursiveB<A, B>(ref A a, ref B b, ref PropertyInfo[] aPIArr, ref PropertyInfo[] bPIArr, int i = 0)
+        {
+            if (i <= bPIArr.Length - 1)
+            {
+                PropertyInfo pi = bPIArr[i];
+                _recursiveA(ref a, ref b, ref aPIArr, ref pi);
+                _recursiveB(ref a, ref b, ref aPIArr, ref bPIArr, ++i);
+            }
+        }
+
+
+        private void _recursiveA<A, B>(ref A a, ref B b, ref PropertyInfo[] aPIArr, ref PropertyInfo bPI, int i = 0)
+        {
+            if (i <= aPIArr.Length - 1)
+            {
+                if (
+                    bPI.Name == aPIArr[i].Name 
+                    && 
+                    bPI.GetType() == aPIArr[i].GetType()
+                    )
+                {
+                    if(aPIArr[i].GetValue(a) != null)
+                    bPI.SetValue(b, aPIArr[i].GetValue(a));
+                    return;
+                }
+                _recursiveA(ref a, ref b, ref aPIArr, ref bPI, ++i);
+            }
+        }
+
+
+    }
+}
---
+++ MilosAPI/MapperStatic.cs
@@ -0,0 +1,63 @@
+﻿using System.Reflection;
+using System.Threading;
+
+namespace MilosAPI
+{
+    static public class MapperStatic
+    {
+        /// <summary>
+        /// Mapping properties with identical name and type.
+        /// </summary>
+        /// <param name="_a">Source</param>
+        /// <param name="_b">Destination</param>
+        static public void Copy<Source, Target>(Source _a, Target _b)
+        {
+            object locker = new object();
+            Monitor.Enter(locker);
+            try
+            {
+                PropertyInfo[] sourceInfoArray = _a.GetType().GetProperties();
+                PropertyInfo[] targetInfoArray = _b.GetType().GetProperties();
+
+                _recursiveA(ref _a, ref _b, ref sourceInfoArray, ref targetInfoArray);
+            }
+            finally
+            {
+                Monitor.Exit(locker);
+            }
+        }
+
+
+        static private void _recursiveA<Source, Target>(ref Source source, ref Target target, ref PropertyInfo[] sourceInfoArray, ref PropertyInfo[] targetInfoArray, int index = 0)
+        {
+            if (index <= targetInfoArray.Length - 1)
+            {
+                PropertyInfo pi = targetInfoArray[index];
+                _recursiveB(ref source, ref target, ref sourceInfoArray, ref pi);
+                _recursiveA(ref source, ref target, ref sourceInfoArray, ref targetInfoArray, ++index);
+            }
+        }
+
+
+        static private void _recursiveB<Source, Target>(ref Source sourceObj, ref Target targetObj, ref PropertyInfo[] sourceInfoArray, ref PropertyInfo targetInfo, int index = 0)
+        {
+            if (index <= sourceInfoArray.Length - 1)
+            {
+                if (
+                    targetInfo.Name == sourceInfoArray[index].Name 
+                    && 
+                    targetInfo.GetType() == sourceInfoArray[index].GetType()
+                    )
+                {
+                    if(sourceInfoArray[index].GetValue(sourceObj) != null)
+                    targetInfo.SetValue(targetObj, sourceInfoArray[index].GetValue(sourceObj));
+                    return;
+                }
+                _recursiveB(ref sourceObj, ref targetObj, ref sourceInfoArray, ref targetInfo, ++index);
+            }
+        }
+
+
+    }
+}
+
