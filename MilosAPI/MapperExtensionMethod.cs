12374b864e4c0bde62425bdb4fd1fecbf24e8e1b
cembosaurus <milos.bencek@gmail.com>
2019-05-01 11:56:15 +1000

    01052019

--- MilosAPI/MapperExtensionMethod.cs
+++ MilosAPI/MapperExtensionMethod.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MilosAPI
+﻿namespace MilosAPI
 {
     public static class MapperExtensionMethod
     {
